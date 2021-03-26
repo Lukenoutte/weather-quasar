103e31a2e8b97b120f2a67f1d01b7be4d6313e4f
Lukenoutte <lukenoutte@gmail.com>
2021-03-26 16:16:42 -0300

    headers 2

--- src/store/data/actions.js
+++ src/store/data/actions.js
@@ -4,13 +4,7 @@ export function getData({ commit }, city) {
   let key = "33d917e3";
   let url = "https://api.hgbrasil.com/weather?format=json-cors&key=" + key;
 
-  let headers = {
-    "Access-Control-Allow-Headers":
-      "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin",
-    "Content-Type": "application/json", 
-    "Access-Control-Allow-Methods": "POST, OPTIONS",
-    "Access-Control-Max-Age": "8640"
-  };
+  let headers = {"Access-Control-Allow-Origin": "*"};
 
   if (city) {
     url = url + "&city_name=" + city;
