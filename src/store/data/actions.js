a18a917151f154c3f66fdf4a0d68719e3208efa4
Lukenoutte <lukenoutte@gmail.com>
2021-03-26 19:57:56 -0300

    remove header

--- src/store/data/actions.js
+++ src/store/data/actions.js
@@ -3,13 +3,13 @@ import { axios } from "../../boot/axios";
 
 export function getData({ commit }, city) {
   let key = "33d917e3";
-  let cors = "https://cors-anywhere.herokuapp.com/";
-  let url = cors + "https://api.hgbrasil.com/weather?format=json-cors&key=" + key;
+
+  let url = "https://api.hgbrasil.com/weather?format=json-cors&key=" + key;
 
   if (city) {
     url = url + "&city_name=" + city;
   }else{
-    url = cors +  "https://api.hgbrasil.com/weather?format=json-cors";
+    url = "https://api.hgbrasil.com/weather?format=json-cors";
   }
 
   axios({ baseURL: url })
