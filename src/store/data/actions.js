dae0ac05da7eef230cd02d4845f1159cad6bce48
Lukenoutte <lukenoutte@gmail.com>
2021-03-26 16:10:36 -0300

    axios headers

--- src/store/data/actions.js
+++ src/store/data/actions.js
@@ -1,23 +1,30 @@
 import { axios } from "../../boot/axios";
 
-
 export function getData({ commit }, city) {
   let key = "33d917e3";
   let url = "https://api.hgbrasil.com/weather?format=json-cors&key=" + key;
 
+  let headers = {
+    "Access-Control-Allow-Headers":
+      "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin",
+    "Content-Type": "application/json", 
+    "Access-Control-Allow-Methods": "POST, OPTIONS",
+    "Access-Control-Max-Age": "8640"
+  };
+
   if (city) {
     url = url + "&city_name=" + city;
-  }else{
+  } else {
     url = "https://api.hgbrasil.com/weather?format=json-cors";
   }
 
-  axios({ baseURL: url })
+  axios({headers, baseURL: url })
     .then(response => {
       commit("SET_WEATHER_DATA", response.data.results);
     })
     .catch(err => {
       if (err) {
-        this.$router.push({ path: '/404' })
+        this.$router.push({ path: "/404" });
       }
     });
 }
